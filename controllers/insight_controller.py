6e12deffca44a02f541bbe063de92711243f5108
Wessel Kuijpers <wrkuijpers@outlook.com>
2019-05-24 12:34:49 +0200

    split backend service

--- controllers/insight_controller.py
+++ controllers/insight_controller.py
@@ -5,12 +5,6 @@ from flask import Blueprint, jsonify, render_template
 # register the blueprint
 insight_controller = Blueprint('insight_controller', __name__)
 
-# route: '/insight/'
-# returns: insight page
-@insight_controller.route('/')
-def insight_index():
-    return render_template('insight/index.html')
-
 # route: '/insight/data/history'
 # returns: json, fetched from the 'static/model_history/history.json' -file
 @insight_controller.route('/data/history')
--- router.py
+++ router.py
@@ -1,8 +1,5 @@
 from controllers.insight_controller import insight_controller
-from controllers.page_controller import page_controller
 from controllers.predict_controller import predict_controller
-from controllers.rate_controller import rate_controller
-from controllers.translate_controller import translate_controller
 
 # class containing methods for registering th router
 class Router:
@@ -13,8 +10,5 @@ class Router:
 
         # register all the required functional blueprints pointing to the controllers
         # TODO: find some way to do this dynamically
-        app.register_blueprint(page_controller, url_prefix="/")
         app.register_blueprint(predict_controller, url_prefix="/predict")
-        app.register_blueprint(rate_controller, url_prefix='/rate')
-        app.register_blueprint(translate_controller, url_prefix='/translate')
         app.register_blueprint(insight_controller, url_prefix='/insight')
